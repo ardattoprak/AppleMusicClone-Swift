a6996a0cf55a5b4ee9bc951df7c724a1ce0b8192
ardatoprak <ardattoprak@gmail.com>
2023-01-23 16:54:30 +0300

    touchDown and touchUpInside functions added

--- Apple Music Clone/ViewController.swift
+++ Apple Music Clone/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
             }
         }
     }
-
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -51,6 +51,58 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Actions
+    @IBAction func touchDown(_ button: UIButton) {
+        let buttonBackgroundView: UIView
+        
+        switch button {
+        case reverseButton:
+            buttonBackgroundView = reverseBackGroundView
+            
+        case playPauseButton:
+            buttonBackgroundView = playBackGroundView
+            
+        case forwardButton:
+            buttonBackgroundView = forwardBackGroundView
+            
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.25) {
+            buttonBackgroundView.alpha = 0.3
+            button.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }
+    }
+    
+    @IBAction func touchUpInside(_ button: UIButton) {
+        
+        let buttonBackgroundView: UIView
+        
+        switch button {
+        case reverseButton:
+            buttonBackgroundView = reverseBackGroundView
+            
+        case playPauseButton:
+            buttonBackgroundView = playBackGroundView
+            
+        case forwardButton:
+            buttonBackgroundView = forwardBackGroundView
+            
+        default:
+            return
+        }
+        
+        UIView.animate(withDuration: 0.25) {
+            buttonBackgroundView.alpha = 0
+            buttonBackgroundView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            button.transform = .identity
+        } completion: { _ in
+            buttonBackgroundView.transform = .identity
+        }
+        
+    }
+    
+    
     @IBAction func playPauseButtonTapped(_ button: UIButton) {
         if isPlaying {
             UIView.animate(withDuration: 0.5) {
@@ -65,6 +117,7 @@ class ViewController: UIViewController {
         isPlaying.toggle()
         // Trueysa false falsesa true yapar
     }
-
+    
+    
+    
 }
-
